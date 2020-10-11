b30748ccb539d4b6c646bc6ca03486249fcc8330
hmunuy <iamhmunuy@gmail.com>
2020-10-12 03:56:50 +0700

    rh

--- fitm/urls.py
+++ fitm/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('page3/', views.page3, name='page3'),
     path('report/', views.report, name='report'),
     path('login/', views.login, name='login'),
+    path('home/', views.login, name='home'),
     path('logout/', views.logout, name='logout')
 ]
