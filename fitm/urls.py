e936a5ee7e37a01eeab0fd75c7a584b067b0cd54
hmunuy <iamhmunuy@gmail.com>
2020-10-14 15:38:00 +0700

    rweg

--- fitm/urls.py
+++ fitm/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('page3/', views.page3, name='page3'),
     path('page4/', views.page4, name='page4'),
     path('report/', views.report, name='report'),
+    path('register/', views.register, name='register'),
     # path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout')
 ]
