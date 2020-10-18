82602a580ab9b4c9758bf80268f0246a3b708f34
hmunuy <iamhmunuy@gmail.com>
2020-10-19 03:19:09 +0700

    dfh

--- monitoring/views.py
+++ monitoring/views.py
@@ -58,6 +58,7 @@ def home(request):
 
 def monitor(request):
     username = request.session['username']
+    return render(request,'monitor.html')
     # device = request.POST('device')
        
     # try:
