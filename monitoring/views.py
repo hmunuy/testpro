1c680a74ac1b34a206c477e7e2ae8a5063a3387d
hmunuy <iamhmunuy@gmail.com>
2020-10-19 03:01:52 +0700

    drh

--- monitoring/views.py
+++ monitoring/views.py
@@ -58,7 +58,7 @@ def home(request):
 
 def monitor(request):
     username = request.session['username']
-    device = request.POST.dict.get('device',False)
+    device = request.POST.dict.post('device',False)
        
     
     
