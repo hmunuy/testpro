ad2d348833fcaa038b50b5904f5d24d33eb44df5
hmunuy <iamhmunuy@gmail.com>
2020-10-18 22:56:21 +0700

    zdfb

--- monitoring/views.py
+++ monitoring/views.py
@@ -115,7 +115,7 @@ def addUser(request):
             last_name = lastname
             )
         user.save()
-        messages.info(request,'ลงทะเบียนสำเร็จ)
+        messages.info(request,'ลงทะเบียนสำเร็จ')
         return redirect('/registeradmin/')
 
 def login(request):
