12f8024888d4c1c1ccea4e52eed0a809a5875338
hmunuy <iamhmunuy@gmail.com>
2020-10-20 05:09:33 +0700

    fdgnh

--- monitoring/views.py
+++ monitoring/views.py
@@ -105,14 +105,19 @@ def main1(request):
 
 def topology(request):
     username = request.session['username']
-    serach = request.get['serach']
-    if username != '' :
-        return render(request,'home.html')
+    
     
     # if request.POST['search'] =='' :
     #     device = request.POST['search']
     #     return render(request,'/infodevice/',device)
     return render(request,'topology.html',{'username':username})
+
+def topology_serach(request):
+    username = request.session['username']
+    serach = request.get['serach']
+    if username != '' :
+        return render(request,'home.html')
+
 
 def infodevice(request):
     username = request.session['username']
