421ff1fb18b35c6d9b997a929649c57811dc46f5
hmunuy <iamhmunuy@gmail.com>
2020-10-19 22:35:27 +0700

    dh

--- monitoring/views.py
+++ monitoring/views.py
@@ -49,12 +49,12 @@ def home(request):
     #     r = requests.post(url, headers=headers , data = {'message':msg})
       
 
-def wlc(request):
-    butthon = request.POST['butthon']
-    if butthon == 'butthon' :
-        num_wlc = 40
-        # wlc_data = list(snmp_ap.objects.filter(numuser_wlc = num_wlc))
-        return render(request,'monitor.html',{'wlc_data':num_wlc})
+# def wlc(request):
+#     butthon = request.POST['butthon']
+#     if butthon == 'butthon' :
+#         num_wlc = 40
+#         # wlc_data = list(snmp_ap.objects.filter(numuser_wlc = num_wlc))
+#         return render(request,'monitor.html',{'wlc_data':num_wlc})
 
 
     
