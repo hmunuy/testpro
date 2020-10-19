54b196cde6211e7c4d2a5c2d3a2630d004afcf60
hmunuy <iamhmunuy@gmail.com>
2020-10-20 04:32:43 +0700

    th

--- monitoring/views.py
+++ monitoring/views.py
@@ -59,7 +59,7 @@ def monitor(request):
     data = list(snmp_data.objects.all().distinct())
     data2 = list(snmp_ap.objects.all().distinct())
     if username != "" :
-       return render(request,'monitor.html',{'data':data},{'data2':data2})
+       return render(request,'monitor.html',{'data':data,'data2':data2})
     else:
         return render(request,'registeradmin.html')
     return render(request, 'monitor.html', {"username" : username})
