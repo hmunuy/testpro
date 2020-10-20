ecbdaeb1108e4f1930ccb72128b513c09720d373
hmunuy <iamhmunuy@gmail.com>
2020-10-21 00:47:03 +0700

    d

--- monitoring/views.py
+++ monitoring/views.py
@@ -66,7 +66,8 @@ def monitor(request):
     # x_time_sum = x_time.strftime('%d:'+'%A:'+'%B:'+'%Y:'+'%H:'+'%M')
     # data2 = snmp_ap.objects.all().filter(insert_time=x_time_sum).order_by('-numuser_wlc')
     data2 = snmp_ap.objects.values('numuser_wlc')
-    for qry in data2 :
+    y=data2.query
+    for qry in y :
         num = qry.numuser_wlc
         sum_user += num
     x_sum = sum_user
