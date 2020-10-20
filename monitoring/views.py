eabe26b18528ba3d9d678d252e62264c5da7aa51
hmunuy <iamhmunuy@gmail.com>
2020-10-20 16:58:46 +0700

    dgfbkm

--- monitoring/views.py
+++ monitoring/views.py
@@ -59,10 +59,11 @@ def monitor(request):
     username = request.session['username']
     data = list(snmp_data.objects.all().distinct())
     x_time = datetime.datetime.now()
-    x_time_sum = x_time.strftime('%d'+'%A:'+'%B:'+'%Y:'+'%H:'+'%M')
+    x_time_sum = x_time.strftime('%d:'+'%A:'+'%B:'+'%Y:'+'%H:'+'%M')
     data2 = snmp_ap.objects.all().filter(insert_time=x_time_sum).order_by('-numuser_wlc')
     if username != "" :
-       return render(request,'monitor.html',{'data':data,'data2':x_time_sum})
+       return render(request,'monitor.html',{'data':data,'data2':    data2 = snmp_ap.objects.all().filter(insert_time=x_time_sum).order_by('-numuser_wlc')
+})
     else:
         return render(request,'registeradmin.html')
     return render(request, 'monitor.html', {"username" : username})
