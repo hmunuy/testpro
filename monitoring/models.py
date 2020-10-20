49ce1a53d36ccdc553a13b67e10a7ea2194b73d8
hmunuy <iamhmunuy@gmail.com>
2020-10-21 02:58:24 +0700

    gfn

--- monitoring/models.py
+++ monitoring/models.py
@@ -9,7 +9,19 @@ class Host(models.Model):
     uptime = models.CharField(max_length=200)
     insert_time = models.CharField(max_length=200)
 
+class Host_report(models.Model):
+    hostname = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    uptime = models.CharField(max_length=200)
+    insert_time = models.CharField(max_length=200)
+
 class snmp_data(models.Model):
+    ip_hostname = models.CharField(max_length=200)
+    hostname = models.CharField(max_length=200)
+    interface_snmp = models.CharField(max_length=200)
+    status_snmp = models.CharField(max_length=200)
+
+class snmp_data_report(models.Model):
     ip_hostname = models.CharField(max_length=200)
     hostname = models.CharField(max_length=200)
     interface_snmp = models.CharField(max_length=200)
@@ -17,9 +29,27 @@ class snmp_data(models.Model):
     
     
 class hostname(models.Model):
+    ip_hostname = models.CharField(max_length=200)
     hostname = models.CharField(max_length=200)
+    cpu_temp = models.IntegerField()
+    ram_process = models.IntegerField()
+
+class hostname_report(models.Model):
+    ip_hostname = models.CharField(max_length=200)
+    hostname = models.CharField(max_length=200)
+    cpu_temp = models.IntegerField()
+    ram_process = models.IntegerField()
+
 
 class snmp_ap(models.Model):
+    ip_hostname = models.CharField(max_length=200)
+    hostname = models.CharField(max_length=200)
+    numuser_wlc = models.IntegerField()
+    insert_time = models.CharField(max_length=200)
+
+class snmp_ap_report(models.Model):
+    ip_hostname = models.CharField(max_length=200)
+    hostname = models.CharField(max_length=200)
     numuser_wlc = models.IntegerField()
     insert_time = models.CharField(max_length=200)
 
