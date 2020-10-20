3f1b028b59b1e7d27db958b67e8fd1ef92c39675
hmunuy <iamhmunuy@gmail.com>
2020-10-21 00:40:08 +0700

    fdtj

--- monitoring/views.py
+++ monitoring/views.py
@@ -65,9 +65,10 @@ def monitor(request):
     # x_time = datetime.datetime.now()
     # x_time_sum = x_time.strftime('%d:'+'%A:'+'%B:'+'%Y:'+'%H:'+'%M')
     # data2 = snmp_ap.objects.all().filter(insert_time=x_time_sum).order_by('-numuser_wlc')
-    data2 = snmp_ap.objects.all()
+    data2 = snmp_ap.objects.values('numuser_wlc')
     for qry in data2 :
         sum_user = 0
+        num = 0
         num = qry.numuser_wlc
         sum_user = sum_user + num
     x_sum = sum_user
