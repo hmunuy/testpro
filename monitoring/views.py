fb3030ec8facbac178f4d004f032f9b392288080
hmunuy <iamhmunuy@gmail.com>
2020-10-20 14:40:52 +0700

    dfghj

--- monitoring/views.py
+++ monitoring/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from .models import Host
 from .models import snmp_ap
 from .models import snmp_data
+import datetime
 # from .models import snmpdata
 import requests
 from django.http import HttpResponse
@@ -57,7 +58,7 @@ def registeradmin(request):
 def monitor(request):
     username = request.session['username']
     data = list(snmp_data.objects.all().distinct())
-    data2 = snmp_ap.objects.all().filter(id=1).order_by('-numuser_wlc')
+    data2 = snmp_ap.objects.all().filter(insert_time='2020-10-20 04:08:24.180460').order_by('-numuser_wlc')
     if username != "" :
        return render(request,'monitor.html',{'data':data,'data2':data2})
     else:
