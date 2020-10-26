dbaf5aeca48cc3926558d1d14cebf9561cfb9b22
hmunuy <iamhmunuy@gmail.com>
2020-10-27 01:22:26 +0700

    fh

--- monitoring/views.py
+++ monitoring/views.py
@@ -46,7 +46,7 @@ def home(request):
     data = Host.objects.values('hostname','description','uptime','insert_time').distinct()
     data2 = snmp_ap.objects.all()
     data3 = hostname.objects.all()
-    data4 = in_out.objects.all()
+    data4 = in_out.objects.filter(ip_hostname='10.99.0.1')
 
     sum_user = 0
     num_user = 0
