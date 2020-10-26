908fbedaa625250fe5a79d8a96cf9a53e784c8ea
hmunuy <iamhmunuy@gmail.com>
2020-10-27 00:00:17 +0700

    gh

--- monitoring/views.py
+++ monitoring/views.py
@@ -112,7 +112,7 @@ def monitor(request):
         
     if username != "" :
        return render(request,'monitor.html',{'data':data,'data2':x_sum,'data3':data3,'data4':data4}) 
-       return render(request,'/home/',sum_in,sum_out) 
+       return redirect('/home/',sum_in,sum_out) 
     else:
         return render(request,'registeradmin.html')
     return render(request, 'monitor.html', {"username" : username})
